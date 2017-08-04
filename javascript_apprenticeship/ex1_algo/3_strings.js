2826b5aa979f94da751d548cd087c8fcc785ec3d
Clément Sauvage <clement.sauvage@me.com>
2017-08-04 12:43:12 +0200

    Add palindrome and fix typos

--- javascript_apprenticeship/ex1_algo/3_strings.js
+++ javascript_apprenticeship/ex1_algo/3_strings.js
@@ -2,14 +2,20 @@
  * Given a string, reverse each word in a the sentence
  * */
 
- var "Mary would you mary me";
+ let reverse = "Mary would you mary me";
 
  /*
  * Given two strings, return true if they are anagrams of one another
  * */
 
- var wordOne = "Pascal Obispo";
- var wordTwo =  "Pablo Picasso";
+ let wordOne = "Pascal Obispo";
+ let wordTwo =  "Pablo Picasso";
  // => returns true !
 
- 
+
+ /*
+ * Check if a given string is a palindrome "Rotor" is a palindrome. "Race Car" should also be considered a palindrome.
+ * Case sensitivity should be taken into account
+ * */
+
+ let palindrome = "Eh ! ça va la vache";
